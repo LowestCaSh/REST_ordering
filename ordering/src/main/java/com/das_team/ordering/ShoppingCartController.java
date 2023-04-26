aa57a244b4db7c6fa8b81ee0bf2a75abea8c31c9
LowestCaSh <131364417+LowestCaSh@users.noreply.github.com>
2023-04-26 20:40:21 +0200

    Test2

--- ordering/src/main/java/com/das_team/ordering/ShoppingCartController.java
+++ ordering/src/main/java/com/das_team/ordering/ShoppingCartController.java
@@ -1,15 +1,12 @@
 package com.das_team.ordering;
 
 import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.HttpStatus;
-
 import io.swagger.annotations.ApiOperation;
-
 
 @RestController
 public class ShoppingCartController {
