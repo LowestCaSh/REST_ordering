5e17d90a47119797ee46f7adc112fb1bcbef57b9
LowestCaSh <131364417+LowestCaSh@users.noreply.github.com>
2023-04-26 17:42:52 +0200

    Added ApiInfo

--- ordering/src/main/java/com/das_team/ordering/SwaggerConfig.java
+++ ordering/src/main/java/com/das_team/ordering/SwaggerConfig.java
@@ -5,7 +5,13 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
+
 
 @EnableSwagger2
 public class SwaggerConfig {
@@ -16,6 +22,20 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.das_team.ordering"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(metaInfo());
     }
+	
+	private ApiInfo metaInfo() {
+	    return new ApiInfo(
+	    		"Endpoints for Orders and ShoppingCarts",
+	            "Returns Information about Orders and ShoppingCarts for our REST API study project",
+	            "1.0",
+	            "Terms of Service",
+	            new Contact("Lukas und Fabian", "E-Mails", "luko0005 und faam0001"),
+	            "No License",
+	            "No License url",
+	            Collections.emptyList()
+	    );
+	}
 }
