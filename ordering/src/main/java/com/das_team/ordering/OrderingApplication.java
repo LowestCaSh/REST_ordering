40378b6599d733688e30dcef19942df89ec09be2
LowestCaSh <131364417+LowestCaSh@users.noreply.github.com>
2023-04-26 17:14:29 +0200

    Seperated SwaggerConfig and MainApp

--- ordering/src/main/java/com/das_team/ordering/OrderingApplication.java
+++ ordering/src/main/java/com/das_team/ordering/OrderingApplication.java
@@ -3,25 +3,10 @@ package com.das_team.ordering;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
-@EnableSwagger2
 public class OrderingApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(OrderingApplication.class, args);	
 	}
-	
-	public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.das_team.ordering"))
-                .paths(PathSelectors.any())
-                .build();
-    }
 }
