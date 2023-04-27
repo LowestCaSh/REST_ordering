6037f9df970df3270f06c89d58b978fd811c26f7
LowestCaSh <131364417+LowestCaSh@users.noreply.github.com>
2023-04-27 17:12:50 +0200

    PostMappings for Details & removed Controllers
    Added PostMappings for OrderDetail & ShoppingCartDetail.
    Removed their Controllers (not necessary)

---
+++ ordering/src/main/java/com/das_team/ShoppingCart.java
@@ -0,0 +1,67 @@
+package com.das_team.ordering;
+
+import java.util.List;
+
+public class ShoppingCart {
+
+    private int cartId;
+    private int customerId;
+    private List<ShoppingCartDetail> cartDetails;
+    private float totalSum;
+    private String lastUpdated;
+
+    public ShoppingCart(int cartId,int customerId, List<ShoppingCartDetail> cartDetails, float totalSum, String lastUpdated) {
+        this.cartId = cartId;
+        this.customerId = customerId;
+        this.cartDetails = cartDetails;
+        this.totalSum = totalSum;
+        this.lastUpdated = lastUpdated;
+        
+    }
+    
+    //Default Constructor needed for @PostMappings
+  	public ShoppingCart() {
+  		
+  	}
+
+    public int getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(int cartId) {
+        this.cartId = cartId;
+    }
+    
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public List<ShoppingCartDetail> getCartDetails() {
+		return cartDetails;
+	}
+
+	public void setCartDetails(List<ShoppingCartDetail> cartDetails) {
+		this.cartDetails = cartDetails;
+	}
+
+	public float getTotalSum() {
+        return totalSum;
+    }
+
+    public void setTotalSum(float totalSum) {
+        this.totalSum = totalSum;
+    }
+    
+    public String getLastUpdated() {
+        return lastUpdated;
+    }
+    
+    public void setLastUpdated(String lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+        
+}
---
+++ ordering/src/main/java/com/das_team/ShoppingCartDetail.java
@@ -0,0 +1,74 @@
+package com.das_team.ordering;
+
+public class ShoppingCartDetail {
+	
+	private int cartId;
+	private int productId;
+	private String productName;
+	private String unit;
+	private float unitprice;
+	private int quantity;
+	
+	public ShoppingCartDetail(int cartId, int productId, String productName, String unit, float unitprice, int quantity) {
+		this.cartId = cartId;
+		this.productId = productId;
+		this.productName = productName;
+		this.unit = unit;
+		this.unitprice = unitprice;
+		this.quantity = quantity;
+	}
+	
+	//Default Constructor needed for @PostMappings
+	public ShoppingCartDetail() {
+		
+	}
+
+	public int getCartId() {
+		return cartId;
+	}
+
+	public void setCartId(int cartId) {
+		this.cartId = cartId;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	public float getUnitprice() {
+		return unitprice;
+	}
+
+	public void setUnitprice(float unitprice) {
+		this.unitprice = unitprice;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+}
