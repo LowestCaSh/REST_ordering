316318ede0a0c4cf30ce4722039d0ce244829490
LowestCaSh <131364417+LowestCaSh@users.noreply.github.com>
2023-06-01 13:32:33 +0200

    Added new Class for POST & removed from main

---
+++ ordering/src/main/java/com/das_team/ordering/POST_Request.java
@@ -0,0 +1,50 @@
+package com.das_team.ordering;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class POST_Request {
+
+	public static void main(String[] args) {
+		//POST to another Group
+				String requestBody = "{\"name\":\"Das coole Produkt3\",\"brand\":\"Boss\",\"color\":\"Coool\",\"size\":\"Gigantisch\",\"weight\":99.99,\"description\":\"riesig\",\"category\":\"Nicht nötig\",\"prices\":[{\"validFrom\":\"07.05.2021\",\"validTo\":\"07.05.2022\",\"price\":\"902.00\"}],\"discounted\":true,\"discountAmount\":20,\"stock\":117,\"packagingSize\":\"\",\"packagingWeight\":\"Gewicht7\",\"discontinued\":false,\"modelnumber\":\"54r12opklhdhl5ug\"}"; // Customize the request body here 
+				String url = "http://192.168.0.100:8000/v2/products/create";
+				try {
+		            URL apiUrl = new URL(url);
+		            HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
+		            connection.setRequestMethod("PUT");
+		            connection.setRequestProperty("Content-Type", "application/json");
+		            connection.setDoOutput(true);
+
+		            OutputStream outputStream = connection.getOutputStream();
+		            outputStream.write(requestBody.getBytes());
+		            outputStream.flush();
+
+		            int statusCode = connection.getResponseCode();
+
+		            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		            String line;
+		            StringBuilder response = new StringBuilder();
+
+		            while ((line = reader.readLine()) != null) {
+		                response.append(line);
+		            }
+
+		            reader.close();
+		            connection.disconnect();
+
+		            // Process the response as needed
+		            System.out.println("Response Code: " + statusCode);
+		            System.out.println("Response Body: " + response.toString());
+		        } catch (IOException e) {
+		            e.printStackTrace();
+		        }
+
+	}
+
+}
